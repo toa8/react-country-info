af57369a8f8557df00c22799a896cf256e317199
Talha Orhun Akdas <talhaorhunakdas@gmail.com>
2023-07-31 18:46:28 +0300

    feat: fetched data from api and consumed in countrycard comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import React from "react";
+
+import axios from "axios";
+
+// Components
 import Navbar from "./components/Navbar";
+import SearchFilterContent from "./components/SearchFilterContent";
+import CountryCard from "./components/CountryCard";
 
 const App = () => {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://restcountries.com/v3.1/all`);
+        setData(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <SearchFilterContent />
+      <div className="mt-10 flex items-center justify-center flex-wrap gap-8 w-4/5 mx-auto">
+        {data.map((item, idx) => (
+          <CountryCard data={item} key={idx} />
+        ))}
+      </div>
+    </>
   );
 };
 
---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+const CountryCard = ({ data, idx }) => {
+  return (
+    <div className="w-1/4 h-[80vh] rounded-lg" key={idx}>
+      {data ? (
+        <>
+          <div className="w-full h-2/4">
+            <img
+              className="w-full h-full rounded-t-lg cursor-pointer"
+              src={data.flags?.png}
+              alt={data.flags?.alt}
+            />
+          </div>
+          <div className=" mt-4 flex gap-2 flex-col ">
+            <p className="font-bold text-3xl text-stone-800 text-center">
+              {data.name?.common}
+            </p>
+            <p className="text-xl p-2 text-stone-700">
+              Capital : {data.capital}
+            </p>
+            <p className="text-xl p-2 text-stone-700">Region: {data.region}</p>
+            <p className="text-xl p-2 text-stone-700">
+              Population: {data.population}
+            </p>
+          </div>
+        </>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
+};
+
+export default CountryCard;
