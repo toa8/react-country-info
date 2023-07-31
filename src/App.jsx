95cdc8f0398face8c195277866aefb28b7ee7258
Talha Orhun Akdas <talhaorhunakdas@gmail.com>
2023-07-31 23:05:35 +0300

    feat: main file cleared

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,11 @@ import React from "react";
 // Components
 import Navbar from "./components/Navbar";
 import SearchFilterContent from "./components/SearchFilterContent";
-import CountryCard from "./components/CountryCard";
-
-import useFetch from "./hooks/useFetch";
+import DisplayCards from "./components/DisplayCards";
 
 const App = () => {
   const [inputValue, setInputValue] = React.useState("");
   const [btnValue, setBtnValue] = React.useState(null);
-  const [url, setUrl] = React.useState("");
-
-  React.useEffect(() => {
-    if (inputValue !== "") {
-      const filteredUrl = `https://restcountries.com/v3.1/name/${inputValue}`;
-      const finalUrl =
-        btnValue !== null
-          ? `https://restcountries.com/v3.1/region/${btnValue}`
-          : filteredUrl;
-      setUrl(finalUrl);
-    } else {
-      const finalUrl =
-        btnValue !== null
-          ? `https://restcountries.com/v3.1/region/${btnValue}`
-          : "https://restcountries.com/v3.1/all";
-      setUrl(finalUrl);
-    }
-  }, [inputValue, btnValue]);
-
-  const { data } = useFetch(url);
 
   return (
     <div className="w-full min-h-[100vh]">
@@ -37,11 +15,7 @@ const App = () => {
         setBtnValue={setBtnValue}
         setInputValue={setInputValue}
       />
-      <div className="flex flex-wrap gap-12 justify-center items-center w-4/5 min-h-[100vh] mx-auto mt-10">
-        {data?.map((country, idx) => (
-          <CountryCard data={country} key={idx} />
-        ))}
-      </div>
+      <DisplayCards inputValue={inputValue} btnValue={btnValue} />
     </div>
   );
 };
---
+++ src/components/DisplayCards.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+// Components
+import CountryCard from "./UI/CountryCard";
+
+import useFetch from "../hooks/useFetch";
+
+const DisplayCards = ({ inputValue, btnValue }) => {
+  const [url, setUrl] = React.useState("");
+
+  React.useEffect(() => {
+    if (inputValue !== "") {
+      const filteredUrl = `https://restcountries.com/v3.1/name/${inputValue}`;
+      const finalUrl =
+        btnValue !== null
+          ? `https://restcountries.com/v3.1/region/${btnValue}`
+          : filteredUrl;
+      setUrl(finalUrl);
+    } else {
+      const finalUrl =
+        btnValue !== null
+          ? `https://restcountries.com/v3.1/region/${btnValue}`
+          : "https://restcountries.com/v3.1/all";
+      setUrl(finalUrl);
+    }
+  }, [inputValue, btnValue]);
+
+  const { data } = useFetch(url);
+  return (
+    <div className="flex flex-wrap gap-12 justify-center items-center w-4/5 min-h-[100vh] mx-auto mt-10">
+      {data?.map((country, idx) => (
+        <CountryCard data={country} key={idx} />
+      ))}
+    </div>
+  );
+};
+
+export default DisplayCards;
